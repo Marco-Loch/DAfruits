f67ec6357a72c2ecbb92cc8969dd5b7bc2a70d61
Marco Loch <cn_macro@web.de>
2024-04-02 14:29:38 +0200

    initial

---
+++ src/app/fruit-container/fruit-container.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fruit-container',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './fruit-container.component.html',
+  styleUrl: './fruit-container.component.scss'
+})
+export class FruitContainerComponent {
+
+}
